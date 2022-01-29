33a7b29e4052b91483e011da841fbb213692a261
EstefaniRoman <98669619+EstefaniRoman@users.noreply.github.com>
2022-01-29 16:06:04 -0600

    Tarea 2

---
+++ 201902617_tarea2/201902617_TIENDA/201902617/Form1.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lbsazucar = New System.Windows.Forms.TextBox()
+        Me.lbsfrijol = New System.Windows.Forms.TextBox()
+        Me.lbsarroz = New System.Windows.Forms.TextBox()
+        Me.azucar = New System.Windows.Forms.Label()
+        Me.frijol = New System.Windows.Forms.Label()
+        Me.arroz = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblR5 = New System.Windows.Forms.Label()
+        Me.lblR4 = New System.Windows.Forms.Label()
+        Me.lblR3 = New System.Windows.Forms.Label()
+        Me.lblR2 = New System.Windows.Forms.Label()
+        Me.lblR1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnAceptar = New System.Windows.Forms.Button()
+        Me.BtnLimpiar = New System.Windows.Forms.Button()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.lbsazucar)
+        Me.GroupBox1.Controls.Add(Me.lbsfrijol)
+        Me.GroupBox1.Controls.Add(Me.lbsarroz)
+        Me.GroupBox1.Controls.Add(Me.azucar)
+        Me.GroupBox1.Controls.Add(Me.frijol)
+        Me.GroupBox1.Controls.Add(Me.arroz)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 13)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(455, 183)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos de Ingreso"
+        '
+        'lbsazucar
+        '
+        Me.lbsazucar.Location = New System.Drawing.Point(213, 140)
+        Me.lbsazucar.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.lbsazucar.Name = "lbsazucar"
+        Me.lbsazucar.Size = New System.Drawing.Size(183, 25)
+        Me.lbsazucar.TabIndex = 5
+        Me.lbsazucar.Text = "0"
+        '
+        'lbsfrijol
+        '
+        Me.lbsfrijol.Location = New System.Drawing.Point(213, 99)
+        Me.lbsfrijol.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.lbsfrijol.Name = "lbsfrijol"
+        Me.lbsfrijol.Size = New System.Drawing.Size(183, 25)
+        Me.lbsfrijol.TabIndex = 4
+        Me.lbsfrijol.Text = "0"
+        '
+        'lbsarroz
+        '
+        Me.lbsarroz.Location = New System.Drawing.Point(213, 56)
+        Me.lbsarroz.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.lbsarroz.Name = "lbsarroz"
+        Me.lbsarroz.Size = New System.Drawing.Size(183, 25)
+        Me.lbsarroz.TabIndex = 3
+        Me.lbsarroz.Text = "0"
+        '
+        'azucar
+        '
+        Me.azucar.AutoSize = True
+        Me.azucar.Location = New System.Drawing.Point(47, 149)
+        Me.azucar.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.azucar.Name = "azucar"
+        Me.azucar.Size = New System.Drawing.Size(94, 17)
+        Me.azucar.TabIndex = 2
+        Me.azucar.Text = "Lbs. de Azucar"
+        '
+        'frijol
+        '
+        Me.frijol.AutoSize = True
+        Me.frijol.Location = New System.Drawing.Point(47, 109)
+        Me.frijol.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.frijol.Name = "frijol"
+        Me.frijol.Size = New System.Drawing.Size(81, 17)
+        Me.frijol.TabIndex = 1
+        Me.frijol.Text = "Lbs. de Frijol"
+        '
+        'arroz
+        '
+        Me.arroz.AutoSize = True
+        Me.arroz.Location = New System.Drawing.Point(48, 64)
+        Me.arroz.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.arroz.Name = "arroz"
+        Me.arroz.Size = New System.Drawing.Size(86, 17)
+        Me.arroz.TabIndex = 0
+        Me.arroz.Text = "Lbs. de Arroz"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox2.Controls.Add(Me.lblR5)
+        Me.GroupBox2.Controls.Add(Me.lblR4)
+        Me.GroupBox2.Controls.Add(Me.lblR3)
+        Me.GroupBox2.Controls.Add(Me.lblR2)
+        Me.GroupBox2.Controls.Add(Me.lblR1)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(92, 204)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(349, 237)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Resultados en Quetzales (Q)"
+        '
+        'lblR5
+        '
+        Me.lblR5.AutoSize = True
+        Me.lblR5.Location = New System.Drawing.Point(203, 199)
+        Me.lblR5.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblR5.Name = "lblR5"
+        Me.lblR5.Size = New System.Drawing.Size(27, 17)
+        Me.lblR5.TabIndex = 9
+        Me.lblR5.Text = "0.0"
+        '
+        'lblR4
+        '
+        Me.lblR4.AutoSize = True
+        Me.lblR4.Location = New System.Drawing.Point(203, 157)
+        Me.lblR4.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblR4.Name = "lblR4"
+        Me.lblR4.Size = New System.Drawing.Size(27, 17)
+        Me.lblR4.TabIndex = 8
+        Me.lblR4.Text = "0.0"
+        '
+        'lblR3
+        '
+        Me.lblR3.AutoSize = True
+        Me.lblR3.Location = New System.Drawing.Point(203, 130)
+        Me.lblR3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblR3.Name = "lblR3"
+        Me.lblR3.Size = New System.Drawing.Size(27, 17)
+        Me.lblR3.TabIndex = 7
+        Me.lblR3.Text = "0.0"
+        '
+        'lblR2
+        '
+        Me.lblR2.AutoSize = True
+        Me.lblR2.Location = New System.Drawing.Point(203, 89)
+        Me.lblR2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblR2.Name = "lblR2"
+        Me.lblR2.Size = New System.Drawing.Size(27, 17)
+        Me.lblR2.TabIndex = 6
+        Me.lblR2.Text = "0.0"
+        '
+        'lblR1
+        '
+        Me.lblR1.AutoSize = True
+        Me.lblR1.Location = New System.Drawing.Point(203, 52)
+        Me.lblR1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblR1.Name = "lblR1"
+        Me.lblR1.Size = New System.Drawing.Size(27, 17)
+        Me.lblR1.TabIndex = 5
+        Me.lblR1.Text = "0.0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(17, 199)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "TOTAL"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 157)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Descuento"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 130)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Pago con el IVA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 89)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Valor del IVA"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 52)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Pago antes del IVA"
+        '
+        'BtnAceptar
+        '
+        Me.BtnAceptar.Location = New System.Drawing.Point(59, 51)
+        Me.BtnAceptar.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.BtnAceptar.Name = "BtnAceptar"
+        Me.BtnAceptar.Size = New System.Drawing.Size(139, 35)
+        Me.BtnAceptar.TabIndex = 0
+        Me.BtnAceptar.Text = "Aceptar"
+        Me.BtnAceptar.UseVisualStyleBackColor = True
+        '
+        'BtnLimpiar
+        '
+        Me.BtnLimpiar.Location = New System.Drawing.Point(59, 102)
+        Me.BtnLimpiar.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.BtnLimpiar.Name = "BtnLimpiar"
+        Me.BtnLimpiar.Size = New System.Drawing.Size(139, 35)
+        Me.BtnLimpiar.TabIndex = 1
+        Me.BtnLimpiar.Text = "Limpiar"
+        Me.BtnLimpiar.UseVisualStyleBackColor = True
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Location = New System.Drawing.Point(59, 154)
+        Me.BtnSalir.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(139, 35)
+        Me.BtnSalir.TabIndex = 2
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox3.Controls.Add(Me.BtnSalir)
+        Me.GroupBox3.Controls.Add(Me.BtnLimpiar)
+        Me.GroupBox3.Controls.Add(Me.BtnAceptar)
+        Me.GroupBox3.Location = New System.Drawing.Point(529, 112)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.GroupBox3.Size = New System.Drawing.Size(243, 239)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(806, 467)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Segoe Script", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents azucar As System.Windows.Forms.Label
+    Friend WithEvents frijol As System.Windows.Forms.Label
+    Friend WithEvents arroz As System.Windows.Forms.Label
+    Friend WithEvents lbsazucar As System.Windows.Forms.TextBox
+    Friend WithEvents lbsfrijol As System.Windows.Forms.TextBox
+    Friend WithEvents lbsarroz As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblR5 As System.Windows.Forms.Label
+    Friend WithEvents lblR4 As System.Windows.Forms.Label
+    Friend WithEvents lblR3 As System.Windows.Forms.Label
+    Friend WithEvents lblR2 As System.Windows.Forms.Label
+    Friend WithEvents lblR1 As System.Windows.Forms.Label
+    Friend WithEvents BtnAceptar As System.Windows.Forms.Button
+    Friend WithEvents BtnLimpiar As System.Windows.Forms.Button
+    Friend WithEvents BtnSalir As System.Windows.Forms.Button
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ 201902617_tarea2/201902617_TIENDA/201902617/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Math
+
+Public Class Form1
+
+    Const precioArroz As Double = 2.0
+    Const precioFrijol As Double = 1.75
+    Const precioAzucar As Double = 2.5
+    Const IVA As Double = 0.12
+
+    Dim pagoSinIVA As Double = 0
+    Dim valorIVA As Double = 0
+    Dim pagocConIVA As Double = 0
+    Dim Descuento As Double = 0
+    Dim pagoFinal As Double = 0
+
+
+    Private Sub BtnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnAceptar.Click
+        pagoSinIVA = Round((Val(lbsarroz.Text) * precioArroz) + (Val(lbsfrijol.Text) * precioFrijol) + (Val(lbsazucar.Text) * precioAzucar), 2)
+
+        valorIVA = Round((pagoSinIVA * IVA), 2)
+        pagocConIVA = Round((pagoSinIVA + valorIVA), 2)
+        Descuento = Round((pagocConIVA * 0.025), 3)
+        pagoFinal = Round((pagocConIVA - Descuento), 2)
+
+        lblR1.Text = pagoSinIVA
+        lblR2.Text = valorIVA
+        lblR3.Text = pagocConIVA
+        lblR4.Text = Descuento
+        lblR5.Text = pagoFinal
+    End Sub
+
+    Private Sub BtnLimpiar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnLimpiar.Click
+        pagoSinIVA = 0
+        valorIVA = 0
+        pagocConIVA = 0
+        Descuento = 0
+        pagoFinal = 0
+        lblR1.Text = 0
+        lblR2.Text = 0
+        lblR3.Text = 0
+        lblR4.Text = 0
+        lblR5.Text = 0
+        lbsarroz.Text = 0
+        lbsfrijol.Text = 0
+        lbsazucar.Text = 0
+
+    End Sub
+
+    Private Sub BtnSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSalir.Click
+        If MsgBox("¿Desea Salir?", vbQuestion + vbYesNo, "Pregunta") = vbYes Then
+            End
+        End If
+    End Sub
+
+    Private Sub lbsarroz_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles lbsarroz.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub lbsfrijol_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles lbsfrijol.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub lbsazucar_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles lbsazucar.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    
+End Class
